6f66fba42cb763eb0c23f0314bd5da2cdd88eecb
Michael Sanchez <michaelsanchez42@live.com>
2021-02-14 16:06:25 -0700

    mvp

--- database/migrations/20210214155353_users.js
+++ database/migrations/20210214155353_users.js
@@ -1,11 +1,23 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (users) => {
-    users.increments();
-    users.string("username", 64).notNullable();
-    users.string("hash", 128).notNullable();
-    users.string("department", 64).notNullable();
-    users.unique("username");
-  });
+  return knex.schema
+    .createTable("roles", (tbl) => {
+      tbl.increments();
+
+      tbl.string("name", 128).notNullable().unique();
+    })
+
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 64).notNullable().unique().index();
+      tbl.string("hash", 128).notNullable();
+      tbl.string("department", 64).notNullable();
+      tbl
+        .integer("role")
+        .unsigned()
+        .references("roles.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function (knex) {
